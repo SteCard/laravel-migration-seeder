4e072e496319f4b2d55fee6b59ef315c3ac1aeb3
SteCard <stefanocardillo14@gmail.com>
2023-07-28 19:49:33 +0200

    TrainsController function

--- app/Http/Controllers/TrainsController.php
+++ app/Http/Controllers/TrainsController.php
@@ -9,6 +9,10 @@ use App\Models\Train;
 class TrainsController extends Controller
 {
     public function index(){
-        return view('welcome');
+        // dd(now());
+        $now = date('Y-m-d', strtotime(now()));
+        $trains = Train::where('data_partenza', '>=', $now)->get();
+        
+        return view('welcome', compact($trains));
     }
 }
